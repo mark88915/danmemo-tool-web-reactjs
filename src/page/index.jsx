4a1caea94b12c13bda99872f64426980514fab92
DESKTOP-6PS5KK6\幾花 <mark88915@gmail.com>
2022-07-24 16:38:51 +0800

    將BrowserRouter修改為HashRouter部屬至github pages

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import './index.css';
 import Home from './home/home'
