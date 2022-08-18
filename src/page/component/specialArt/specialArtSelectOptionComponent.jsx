e20a461d9a30ca986ea06c7b66802f47837e0aad
DESKTOP-6PS5KK6\幾花 <mark88915@gmail.com>
2022-08-18 16:22:42 +0800

    必殺技量新增光春技能%數

--- src/page/component/specialArt/specialArtSelectOptionComponent.jsx
+++ src/page/component/specialArt/specialArtSelectOptionComponent.jsx
@@ -4,15 +4,19 @@ const SpecialArtSelectOptionComponent = ({turn, characterNo}) => {
             <option value="0">未行動</option>
             <option value="0%">無增量</option>
             <option value="33%">33%</option>
-            <option value="66%">67%</option>
+            <option value="44%">44%</option>
+            <option value="66%">66%</option>
+            <option value="77%">77%</option>
             <option value="100%">100%</option>
             <option value="111%">111%</option>
             <option value="133%">133%</option>
             <option value="144%">144%</option>
+            <option value="155%">155%</option>
             <option value="166%">167%</option>
             <option value="177%">178%</option>
             <option value="200%">200%</option>
             <option value="211%">211%</option>
+            <option value="222%">222%</option>
             <option value="0">必殺</option>
         </select>
     )
